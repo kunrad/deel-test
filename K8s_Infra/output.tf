cb41ad542ea2b57c78c70eae145a1fe546ff6aa2
Conrad Duru <conrad.duru@geniusavenue.com>
2025-02-07 15:42:06 +0100

    fix: improve deployment

--- K8s_Infra/output.tf
+++ K8s_Infra/output.tf
@@ -1,8 +1,12 @@
-output "cluster_id" {
+output "kubectl_save" {
   value = format("doctl kubernetes cluster kubeconfig save %s", digitalocean_kubernetes_cluster.my_cluster.id)
 }
 
 output "kubeconfig" {
-  value = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].raw_config
+  value     = digitalocean_kubernetes_cluster.my_cluster.kube_config[0].raw_config
   sensitive = true
+}
+
+output "name" {
+
 }
