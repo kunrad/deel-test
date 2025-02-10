de2147f9d95d2dc41d3a657dc08daeaf96020b59
Conrad Duru <conrad.duru@geniusavenue.com>
2025-02-10 08:55:11 +0100

    fix: improve pipeline increase timeout

--- K8s_Infra/provider.tf
+++ K8s_Infra/provider.tf
@@ -1,17 +1,17 @@
 terraform {
-required_version = ">= 1.6.3"
+  required_version = ">= 1.6.3"
 
   backend "s3" {
     endpoints = {
-      s3 = "https://sfo3.digitaloceanspaces.com" 
+      s3 = "https://sfo3.digitaloceanspaces.com"
     }
 
-    bucket = "reverseip-statefile"
-    key    = "${TF_VAR_BACKEND_digitalocean_spaces_key}"
+    bucket = "reverseip-statefiles"
+    key    = TF_VAR_BACKEND_digitalocean_spaces_key
 
-    access_key = "${TF_VAR_BACKEND_digitalocean_spaces_access_key}"
-    secret_key = "${TF_VAR_BACKEND_digitalocean_spaces_secret_key}"
-  
+    access_key = TF_VAR_BACKEND_digitalocean_spaces_access_key
+    secret_key = TF_VAR_BACKEND_digitalocean_spaces_secret_key
+
 
     # Deactivate a few AWS-specific checks
     skip_credentials_validation = true
